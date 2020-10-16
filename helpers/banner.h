f2b096fa320a13cbae56bd9d1003c51a47b86536
Stuart B. Wilkins <swilkins@bnl.gov>
2020-10-16 08:42:54 -0400

    Removed hard coded banner

--- helpers/banner.h
+++ helpers/banner.h
@@ -1,43 +1,6 @@
 #ifndef BANNER_H
 #define BANNER_H
 
-// #define BANNER_LINES 24
-//
-// const char* const banner[] = {
-//     "NOTICE TO USERS",
-//     " ",
-//     "This is a Federal computer system (and/or it is directly connected to a",
-//     "BNL local network system) and is the property of the United States",
-//     "Government. It is for authorized use only. Users (authorized or",
-//     "unauthorized) have no explicit or implicit expectation of privacy.",
-//     " ",
-//     "Any or all uses of this system and all files on this system may be",
-//     "intercepted, monitored, recorded, copied, audited, inspected, and",
-//     "disclosed to authorized site, Department of Energy, and law enforcement",
-//     "personnel, as well as authorized officials of other agencies, both",
-//     "domestic and foreign. By using this system, the user consents to such",
-//     "interception, monitoring, recording, copying, auditing, inspection,",
-//     "and disclosure at the discretion of authorized site or Department of",
-//     "Energy personnel.",
-//     " ",
-//     "Unauthorized or improper use of this system may result in administrative",
-//     "disciplinary action and civil and criminal penalties. By continuing to",
-//     "use this system you indicate your awareness of and consent to these",
-//     "terms and conditions of use.",
-//     " ",
-//     "LOG OFF IMMEDIATELY if you do not agree to the conditions stated in this",
-//     "warning.",
-//     " ",
-//     0
-// };
-//
-// const int banner_n = BANNER_LINES;
-// int tw_banner[BANNER_LINES];
-
-#ifndef BANNER_FILE
-#define BANNER_FILE "/etc/xsecurelock/banner"
-#endif
-
 #define BANNER_MAX_LINES        50
 
 int ReadBannerFile(char **banner, int *num_lines, int max_lines);
