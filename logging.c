dc1fc907ab610ca75c166bf1c50007cec64ded9e
Stuart B. Wilkins <swilkins@bnl.gov>
2020-10-23 09:03:58 -0400

    Added syslog to LogError

--- logging.c
+++ logging.c
@@ -1,15 +1,18 @@
 #include "logging.h"
 
+#ifdef SYSLOG
+#define _GNU_SOURCE
+#include <syslog.h>
+#include <string.h>
+#define SYSLOG_LOG_NAME     "xsecurelock"
+#endif
 #include <errno.h>   // for errno
 #include <stdarg.h>  // for va_end, va_list, va_start
 #include <stdio.h>   // for fputs, stderr, vfprintf, perror, NULL
 #include <time.h>
 #include <unistd.h>
-#ifdef SYSLOG
-#include <syslog.h>
-#define SYSLOG_LOG_NAME     "xsecurelock"
-#endif
 
+#ifndef SYSLOG
 static void PrintLogPrefix(void) {
   time_t t = time(NULL);
   struct tm tm_buf;
@@ -20,12 +23,13 @@ static void PrintLogPrefix(void) {
   }
   fprintf(stderr, "%s%ld xsecurelock: ", s, (long)getpid());
 }
+#endif
 
 void Log(const char *format, ...) {
   va_list args;
   va_start(args, format);
 #ifdef SYSLOG
-  openlog(SYSLOG_LOG_NAME, LOG_PID | LOG_NDELAY | LOG_PERROR, LOG_AUTH);
+  openlog(SYSLOG_LOG_NAME, LOG_PID | LOG_NDELAY | LOG_PERROR, LOG_AUTH);
   vsyslog(LOG_INFO, format, args);
   closelog();
 #else
@@ -40,11 +44,19 @@ void LogErrno(const char *format, ...) {
   int errno_save = errno;
   va_list args;
   va_start(args, format);
+#ifdef SYSLOG
+  char buffer[256];
+  vsnprintf(buffer, sizeof(buffer), format, args);
+  openlog(SYSLOG_LOG_NAME, LOG_PID | LOG_NDELAY | LOG_PERROR, LOG_AUTH);
+  syslog(LOG_INFO, "%s: %s", buffer, strerror(errno_save));
+  closelog();
+#else
   PrintLogPrefix();
   vfprintf(stderr, format, args);
   fputs(": ", stderr);
   errno = errno_save;
   perror(NULL);
+#endif
   va_end(args);
   errno = errno_save;
 }
