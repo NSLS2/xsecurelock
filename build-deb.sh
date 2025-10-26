d6295e61e048418680ffaa61d441d18a1f50e0b1
Stuart B. Wilkins <swilkins@bnl.gov>
2025-10-26 18:36:09 -0400

    Add debug

--- build-deb.sh
+++ build-deb.sh
@@ -2,8 +2,6 @@
 set -e
 GITDESC=$(git describe --abbrev=0)
 VERSION=$(git describe --abbrev=0 | sed -En 's/v([0-9]+\.)([0-9]+\.)?([0-9]+)-.*/\1\2\3/p')
-echo "##[debug] GITDESC = $GITDESC"
-echo "##[debug] VERSION = $VERSION"
 mkdir -p build
 gbp buildpackage \
   --git-ignore-new \
@@ -11,7 +9,8 @@ gbp buildpackage \
   -uc -us \
   --git-export-dir=build \
   --git-tarball-dir=build
+echo $debrelease
 old_debfile=$(ls -1 build/xsecurelock-nsls2_*.deb | head -1)
-new_debfile=$(echo ${old_debfile} | sed "s/\.deb/-$(debrelease).deb/")
+new_debfile=$(echo ${old_debfile} | sed "s/\.deb/-${debrelease}.deb/")
 mv "${old_debfile}" "${new_debfile}"
 
